063ffbede14fac4dd2421878a68c97c583b73cd3
autan.m <autan.m@betimes.biz>
2023-05-11 14:38:40 +0700

    แก้ไข require

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,23 @@
 import { EformModuleConfig } from 'eform-share';
 import { Config } from 'share-ui';
 
+// export const environment = {
+//     production: false,
+//     config: {
+//         baseConfig: {
+//             apiUrl: "http://localhost:14121/api",
+//             reportUrl: "",
+//             resourceUrl: "http://www.online.ccib.go.th/resource/ccib/",
+//             hubUrl: "http://localhost:14121/hub"
+//         },
+//         eFormHost: "http://localhost:52169/api",
+//         recaptcha: {
+//             siteKey: "6LfYkEkaAAAAAMGpXixKjWVrYpWDP8AeDMu_dijr",
+//             secretKey: "6LeV1vQcAAAAAJzMmWwJqq76i_fQuflBIF5upGbB"
+//         }
+//     }
+// };
+
 export const environment = {
     production: false,
     config: {
