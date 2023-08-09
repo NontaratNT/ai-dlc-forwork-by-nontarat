b62f5389c59313456f7c8d67a546a54cb2441fab
Kantapon Hangklang <kd_inlove@hotmail.com>
2023-08-09 21:51:07 +0700

    Add Az Prod Env

---
+++ src/environments/environment.az-prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://calm-desert-0e0968700.3.azurestaticapps.net/',
+    config: {
+        baseConfig: {
+            apiUrl: "https://thaipoliceapimgmt.azure-api.net/ccibazure/api",
+            reportUrl: "http://192.168.10.70/web-report/",
+            resourceUrl: "http://192.168.10.71/resource/ccib/",
+            hubUrl: "http://192.168.10.73/hub"
+        },
+        eFormHost: "https://thaipoliceapimgmt.azure-api.net/eformazure/api",
+        recaptcha: {
+            siteKey: "6LeV1vQcAAAAAAUKdpLxVXjB9OLYR7ddI3sDmP0P",
+            secretKey: "6LeV1vQcAAAAAJzMmWwJqq76i_fQuflBIF5upGbB"
+        }
+    }
+};
