ba23a1daf16d0b3b4bb9747bf02f3b09bcbbfa6c
autan.m <autan.m@betimes.biz>
2023-09-23 16:35:24 +0700

    edit config

--- src/environments/environment.az-prod.ts
+++ src/environments/environment.az-prod.ts
@@ -1,6 +1,23 @@
+// export const environment = {
+//     production: true,
+//     baseUrl: 'https://calm-desert-0e0968700.3.azurestaticapps.net/',
+//     config: {
+//         baseConfig: {
+//             apiUrl: "https://thaipoliceapimgmt.azure-api.net/ccibazure/api",
+//             reportUrl: "https://officer.thaipoliceonline.com/web-report/",
+//             resourceUrl: "https://thaipoliceapimgmt.azure-api.net/eformazure/api/resource/",
+//             hubUrl: "https://police-online-noti-azure.azurewebsites.net/hub"
+//         },
+//         eFormHost: "https://thaipoliceapimgmt.azure-api.net/eformazure/api",
+//         recaptcha: {
+//             siteKey: "6LeV1vQcAAAAAAUKdpLxVXjB9OLYR7ddI3sDmP0P",
+//             secretKey: "6LeV1vQcAAAAAJzMmWwJqq76i_fQuflBIF5upGbB"
+//         }
+//     }
+// };
 export const environment = {
     production: true,
-    baseUrl: 'https://thaipoliceonline.com/',
+    baseUrl: 'https://ashy-sky-0b532af00.3.azurestaticapps.net/',
     config: {
         baseConfig: {
             apiUrl: "https://thaipoliceapimgmt.azure-api.net/ccibazure/api",
