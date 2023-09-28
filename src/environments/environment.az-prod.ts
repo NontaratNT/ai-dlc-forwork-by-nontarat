07f7e06074150fa003ec908bf5789684b76db57f
Kantapon Hangklang <kd_inlove@hotmail.com>
2023-09-28 22:43:24 +0700

    Change api url

--- src/environments/environment.az-prod.ts
+++ src/environments/environment.az-prod.ts
@@ -3,12 +3,12 @@
 //     baseUrl: 'https://calm-desert-0e0968700.3.azurestaticapps.net/',
 //     config: {
 //         baseConfig: {
-//             apiUrl: "https://thaipoliceapimgmt.azure-api.net/ccibazure/api",
+//             apiUrl: "https://api.thaipoliceonline.com/ccibazure/api",
 //             reportUrl: "https://officer.thaipoliceonline.com/web-report/",
-//             resourceUrl: "https://thaipoliceapimgmt.azure-api.net/eformazure/api/resource/",
+//             resourceUrl: "https://api.thaipoliceonline.com/eformazure/api/resource/",
 //             hubUrl: "https://police-online-noti-azure.azurewebsites.net/hub"
 //         },
-//         eFormHost: "https://thaipoliceapimgmt.azure-api.net/eformazure/api",
+//         eFormHost: "https://api.thaipoliceonline.com/eformazure/api",
 //         recaptcha: {
 //             siteKey: "6LeV1vQcAAAAAAUKdpLxVXjB9OLYR7ddI3sDmP0P",
 //             secretKey: "6LeV1vQcAAAAAJzMmWwJqq76i_fQuflBIF5upGbB"
@@ -20,13 +20,13 @@ export const environment = {
     baseUrl: 'https://ashy-sky-0b532af00.3.azurestaticapps.net/',
     config: {
         baseConfig: {
-            apiUrl: "https://thaipoliceapimgmt.azure-api.net/ccibazure/api",
+            apiUrl: "https://api.thaipoliceonline.com/ccibazure/api",
             reportUrl: "https://officer.thaipoliceonline.com/web-report/",
-            resourceUrl: "https://thaipoliceapimgmt.azure-api.net/eformazure/api/resource/",
+            resourceUrl: "https://api.thaipoliceonline.com/eformazure/api/resource/",
             hubUrl: "https://police-online-noti-azure.azurewebsites.net/hub",
             urlgdcc:"https://citizenuat.thaipoliceonline.com/api/ccib/v1.0"
         },
-        eFormHost: "https://thaipoliceapimgmt.azure-api.net/eformazure/api",
+        eFormHost: "https://api.thaipoliceonline.com/eformazure/api",
         recaptcha: {
             siteKey: "6LeV1vQcAAAAAAUKdpLxVXjB9OLYR7ddI3sDmP0P",
             secretKey: "6LeV1vQcAAAAAJzMmWwJqq76i_fQuflBIF5upGbB"
