18b97154a221fd4c7b9d073947ec51ed24f4ed02
bt\nontarat.bt <nontarat.t@betimes.biz>
2024-11-07 01:54:07 +0700

    อัปเดต login thaiId

--- src/app/layout/layout-desktop/components/login-thai-id/login-thai-id.component.ts
+++ src/app/layout/layout-desktop/components/login-thai-id/login-thai-id.component.ts
@@ -7,6 +7,7 @@ import { CookieStorage } from 'src/app/common/cookie';
 import { IAccessToken, UserService } from 'src/app/services/user.service';
 import { UserSettingService } from 'src/app/services/user-setting.service';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-thai-id',
@@ -22,10 +23,10 @@ export class LoginThaiIDComponent implements OnInit {
   deviceInfo = null;
 
   constructor(private userServ: UserService,
-     private userSetting: UserSettingService,
-     private deviceService: DeviceDetectorService,
+    private userSetting: UserSettingService,
+    private deviceService: DeviceDetectorService,
 
-     private thaiidservice:ThaiIDService,private _jwtServ: JwtHelperService,private route: ActivatedRoute, private router: Router) { }
+    private thaiidservice: ThaiIDService, private _jwtServ: JwtHelperService, private route: ActivatedRoute, private router: Router) { }
   // url = 'https://imauth.bora.dopa.go.th/api/v1/oauth2/auth/?response_type=code&client_id=WGYyT3lGeGo3c0Z6SEZSbTBRVktaN1VZbUZaVTJwWno&redirect_uri=https://citizenuat.thaipoliceonline.com/login/thaiD&scope=pid%20th_fullname%20dob%20openid&state=af0ifjsldkj';
   url = 'https://imauth.bora.dopa.go.th/api/v1/oauth2/auth/?response_type=code&client_id=RVJKZXRZWWR5NGIyTE1aYWl0aWFCUDRWYUZPU3lqWTk&redirect_uri=https://thaipoliceonline.go.th/login/thaiD&scope=pid%20th_fullname%20dob%20openid&state=af0ifjsldkj';
 
@@ -36,73 +37,99 @@ export class LoginThaiIDComponent implements OnInit {
       const paramValue = params['code'];
       if (paramValue) {
         // Perform your logic based on the parameter value
-        const param = { 
+        const param = {
           code: paramValue,
-          url_redirect:'https://citizenuat.thaipoliceonline.com/login/thaiD'
-        };
-         this.thaiidservice.thaiIdgettoken(param).subscribe((res) => { 
-          this._userInfo = this._jwtServ.decodeToken(res.id_token);
-          // localStorage.setItem('access_token', res.access_token);
-          // localStorage.setItem('_userInfo', this._userInfo);
-
-          this.Login(this._userInfo.pid, this._userInfo.pid);
+          url_redirect: 'https://thaipoliceonline.go.th/login/thaiD'
         }
-        );
-              }else{
+        this.thaiidservice.thaiIdgettokenGDCC(param).subscribe((res) => {
+          console.log(res);
+          if (!res.IsSuccess) {
+            Swal.fire({
+                title: 'ผิดพลาด!',
+                text: `${res.Message}`,
+                // text: 'ไม่พบข้อมูลของท่านในระบบกรมการปกครอง',
+                icon: 'warning',
+                confirmButtonText: 'ตกลง'
+            }).then((_) => {
+              if(_.isConfirmed){
+                this._isLoading = false;
                 window.location.href = this.url;
               }
+            });
+            return;
+        } else {
+            this._userInfo = this._jwtServ.decodeToken(res.Value.id_token);
+            this.Login(this._userInfo.pid, this._userInfo.pid);
+          }
+        }
+        );
+      } else {
+        window.location.href = this.url;
+      }
     });
-      // const token = "eyJhbGciOiJFUzI1NiIsImtpZCI6IkZZQ1ZGY2wyK0dYM2tTTFA0UFlXWGcvcW42OD0iLCJ0eXAiOiJKV1QifQ.eyJhdF9oYXNoIjoiWEUyTXV6YUItWmlIX3JXai0tRHM1ZyIsImF1ZCI6IjAzYzgxODIwLTFiOTYtNDdhNS1iMDVmLTljNzdhNTU3NTFmNyIsImV4cCI6MTcwNDQzODc2MywiaWF0IjoxNzA0NDM3ODYzLCJpc3MiOiJodHRwczovL2ltYXV0aC5ib3JhLmRvcGEuZ28udGgiLCJzdWIiOiIxNDExMTAwMzAxNDMxIiwicGlkIjoiMTQxMTEwMDMwMTQzMSIsInRoX2Z1bGxuYW1lIjoi4LiZ4Liy4LiiIOC4reC4uOC5gOC4l-C4mSDguKHguLHguYjguIfguKHguLnguKUiLCJkb2IiOiIyNTM5MDkxMiJ9.pUrzzAuiXhuXQPPRzOtNPzZFZDo4sSlZGgZPu7gBP6iYqcrkGTRRm82MWeVmtAPmspaE39zeo2_PbYie3-gOfA";
-      // this._userInfo = this._jwtServ.decodeToken(token);
+    // const token = "eyJhbGciOiJFUzI1NiIsImtpZCI6IkZZQ1ZGY2wyK0dYM2tTTFA0UFlXWGcvcW42OD0iLCJ0eXAiOiJKV1QifQ.eyJhdF9oYXNoIjoiWEUyTXV6YUItWmlIX3JXai0tRHM1ZyIsImF1ZCI6IjAzYzgxODIwLTFiOTYtNDdhNS1iMDVmLTljNzdhNTU3NTFmNyIsImV4cCI6MTcwNDQzODc2MywiaWF0IjoxNzA0NDM3ODYzLCJpc3MiOiJodHRwczovL2ltYXV0aC5ib3JhLmRvcGEuZ28udGgiLCJzdWIiOiIxNDExMTAwMzAxNDMxIiwicGlkIjoiMTQxMTEwMDMwMTQzMSIsInRoX2Z1bGxuYW1lIjoi4LiZ4Liy4LiiIOC4reC4uOC5gOC4l-C4mSDguKHguLHguYjguIfguKHguLnguKUiLCJkb2IiOiIyNTM5MDkxMiJ9.pUrzzAuiXhuXQPPRzOtNPzZFZDo4sSlZGgZPu7gBP6iYqcrkGTRRm82MWeVmtAPmspaE39zeo2_PbYie3-gOfA";
+    // this._userInfo = this._jwtServ.decodeToken(token);
 
-      // console.log(this._userInfo);
-  
-   
+    // console.log(this._userInfo);
+
+
 
   }
 
   Login(usename, password) {
     if (usename && password) {
-        this._isLoading = true;
-        this.userSetting.userSetting.location_name = undefined;
-        // this.userSetting.Save();
-        this.userServ
-            .authenticateThaiID(usename, password, 1.1)
-            .pipe(finalize(() => this._isLoading = false))
-            .subscribe(u => {
-                if (u) {
-                    if (this.isRemember) {
-                        CookieStorage.assignSetting({ RememberLogin: true });
-                    }
+      this._isLoading = true;
+      this.userSetting.userSetting.location_name = undefined;
+      // this.userSetting.Save();
+      this.userServ
+        .authenticateThaiID(usename, password, 1.1)
+        .pipe(finalize(() => this._isLoading = false))
+        .subscribe(u => {
+          if (u) {
+            if (this.isRemember) {
+              CookieStorage.assignSetting({ RememberLogin: true });
+            }
 
-                    CookieStorage.accessToken = ({
-                        Token: u.Token,
-                        RefreshToken: u.RefreshToken
-                    } as IAccessToken);
+            CookieStorage.accessToken = ({
+              Token: u.Token,
+              RefreshToken: u.RefreshToken
+            } as IAccessToken);
 
-                    // this.router.navigate([this.loginServ._successLoginRedirectTo]);
-                    this.CheckDeviceMode();
-                }
-            });
+            // this.router.navigate([this.loginServ._successLoginRedirectTo]);
+            this.CheckDeviceMode();
+          }else{
+            Swal.fire({
+              title: 'ผิดพลาด!',
+              text: 'ไม่พบข้อมูลของท่านในระบบ',
+              icon: 'warning',
+              confirmButtonText: 'ตกลง'
+          }).then((_) => {
+            if(_.isConfirmed){
+              this._isLoading = false;
+              window.location.href = "https://thaipoliceonline.go.th/login/thaiD";
+            }
+          })
+        }
+      });
     }
     else {
-        
+
     }
-}
-CheckDeviceMode() {
-  this.deviceInfo = this.deviceService.getDeviceInfo();
-  const isMobile = this.deviceService.isMobile();
-  const isTablet = this.deviceService.isTablet();
-  const isDesktopDevice = this.deviceService.isDesktop();
-  if (isMobile) {
+  }
+  CheckDeviceMode() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    const isMobile = this.deviceService.isMobile();
+    const isTablet = this.deviceService.isTablet();
+    const isDesktopDevice = this.deviceService.isDesktop();
+    if (isMobile) {
 
       // location.reload();
       this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-          this.router.navigate(["/mobile/track-status?openExternalBrowser=1"]);
+        this.router.navigate(["/mobile/track-status?openExternalBrowser=1"]);
       });
-  } else {
+    } else {
       this.router.navigate(["/main/task-list"]);
+    }
   }
-}
 
 }
--- src/app/services/thai-id.service.ts
+++ src/app/services/thai-id.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {HttpStatusResultValue, req} from 'share-ui';
-import {OnlineCaseInfo} from "../common/@type/online-case";
+import { HttpStatusResultValue, req } from 'share-ui';
+import { OnlineCaseInfo } from "../common/@type/online-case";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { CookieStorage } from '../common/cookie';
@@ -12,31 +12,34 @@ import { EFORM_REQUEST, EformRequestFactory } from 'eform-share';
 export class ThaiIDService {
 
   private apiUrl = 'https://imauth.bora.dopa.go.th/api/v1/oauth2/auth/';
-  constructor(@Inject(EFORM_REQUEST) private _req: EformRequestFactory,private http: HttpClient) {}
+  constructor(@Inject(EFORM_REQUEST) private _req: EformRequestFactory, private http: HttpClient) { }
 
   // Method to make the GET request
   getApiData(): Observable<any> {
     const params = {
       response_type: 'code',
       // client_id: 'WGYyT3lGeGo3c0Z6SEZSbTBRVktaN1VZbUZaVTJwWno',
-      client_id:'RVJKZXRZWWR5NGIyTE1aYWl0aWFCUDRWYUZPU3lqWTk',
+      client_id: 'RVJKZXRZWWR5NGIyTE1aYWl0aWFCUDRWYUZPU3lqWTk',
 
       // redirect_uri: 'https://citizenuat.thaipoliceonline.com/login/thaiD',
-      redirect_uri:'https://thaipoliceonline.go.th/login/thaiD',
+      redirect_uri: 'https://thaipoliceonline.go.th/login/thaiD',
       scope: 'pid th_fullname dob openid',
       state: 'af0ifjsldkj',
     };
     const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded'    });
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
 
     return this.http.get(this.apiUrl, { params, headers });
   }
 
-  public thaiIdgettoken(param: any): Observable<any> {
-    return this._req<any>().api('LoginThaiID/gettokenext')
-    .body(param)
-    .disableCriticalDialogError().post();
+  public thaiIdgettokenGDCC(param: any): Observable<any> {
+    return  this.http.post<any>(`${environment.config.baseConfig.urlgdcceform}/LoginThaiID/gettokenext`, param);
+  }
 
-        
-}
+  public thaiIdgettoken(param: any): Observable<any> {
+    return this._req<any>().api('LoginThaiID/create-user-thaiId')
+      .body(param)
+      .disableCriticalDialogError().post();
+  }
 }
