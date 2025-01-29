e2a8523138856aefb53874b66546024ecc2bb938
Arkrapon P. <Arkrapon.p@betimes.biz>
2025-01-29 11:59:23 +0700

    update icon res

--- src/app/layout/layout-home/components/re-design/formationnews/formationnews.component.ts
+++ src/app/layout/layout-home/components/re-design/formationnews/formationnews.component.ts
@@ -1,68 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-formationnews',
-  templateUrl: './formationnews.component.html',
-  styleUrls: ['./formationnews.component.scss']
+    selector: "app-formationnews",
+    templateUrl: "./formationnews.component.html",
+    styleUrls: ["./formationnews.component.scss"],
 })
 export class FormationnewsComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-    this.setupPagination();
-  }
-  bankContacts = [
-    { name: 'กสิกรไทย', phone: '0-2888-8888 กด 001', logo: 'assets/icon/kbank.png' },
-    { name: 'ธนาคารกรุงไทย', phone: '0-2111-1111 กด 108', logo: 'assets/icon/ktb.png' },
-    { name: 'ธนาคารกรุงศรีอยุธยา', phone: '1572 กด 5', logo: 'assets/icon/bay.png' },
-    { name: 'ธนาคารกรุงเทพ', phone: '1333 หรือ 0-2645-5555 กด * 3', logo: 'assets/icon/bbl.png' },
-    { name: 'ธนาคารไทยพาณิชย์', phone: '0-2777-7575', logo: 'assets/icon/scb.png' },
-    { name: 'ธนาคารทหารไทยธนชาต', phone: '1428 กด 03', logo: 'assets/icon/ttb.png' },
-    { name: 'ธนาคารออมสิน', phone: '1115 กด 6', logo: 'assets/icon/gsb.png' },
-    { name: 'ธนาคารซีไอเอ็มบี ไทย', phone: '0-2626-7777 กด 12', logo: 'assets/icon/cimb.png' },
-    { name: 'ธนาคารไทยเครดิต', phone: '0-2697-5454', logo: 'assets/icon/tcrb.png' },
-    { name: 'ธนาคารแลนด์ แอนด์ เฮ้าส์', phone: '0-2459-0000 กด 8', logo: 'assets/icon/lhb.png' },
-    { name: 'ธนาคารอาคารสงเคราะห์', phone: '0-2645-9000 กด 33', logo: 'assets/icon/ghb.png' },
-    { name: 'ธนาคารเพื่อการเกษตรและสหกรณ์', phone: '0-2888-8888 กด 001', logo: 'assets/icon/baac.png' },
-    { name: 'ธนาคารยูโอบี', phone: '0-2344-9555', logo: 'assets/icon/uob.png' },
-    { name: 'ธนาคารเกียรตินาคินภัทร', phone: '0-2165-5555 กด 6', logo: 'assets/icon/kk.png' },
-    { name: 'ธนาคารทิสโก้', phone: '0-2633-6000 กด * 7', logo: 'assets/icon/tisco.png' },
-    { name: 'ธนาคารไอซีบีซี(ไทย)', phone: '0 2629 5588 กด 4', logo: 'assets/icon/icbc.png' },
-    { name: 'ธนาคารอิสลามแห่งประเทศไทย', phone: '1302 กด 6', logo: 'assets/icon/ibank.png' },
-    { name: 'ทรูมันนี่', phone: '1240 กด 6', logo: 'assets/icon/true.png' },
-    { name: 'ทูซีทูพี(ประเทศไทย)', phone: '0 2026 3000 กด 0', logo: 'assets/icon/2C2P.png' },
-    { name: 'แอดวานซ์ เอ็มเปย์', phone: '0 2078 9299 กด 1', logo: 'assets/icon/mpay.png' },
-    { name: 'ธนาคารไทยไมโคร ดิจิทัล โซลูชั่นส์', phone: '0 2697 5353 กด 0', logo: 'assets/icon/tmc.jpg' },
-    { name: 'แมกซ์ การ์ด', phone: '1614 กด 4', logo: 'assets/icon/max.jpg' },
-  ];
-
-  paginatedNews: any[][] = [];
-  currentPage = 0;
-  itemsPerPage = 3;
-
-  setupPagination(): void {
-    for (let i = 0; i < this.bankContacts.length; i += this.itemsPerPage) {
-      this.paginatedNews.push(this.bankContacts.slice(i, i + this.itemsPerPage));
+    ngOnInit(): void {
+        this.setupPagination();
     }
-  }
+    formationNews = [
+        {
+            category: "ข่าวประชาสัมพันธ์",
+            title: "ถูกแฮกข้อมูลส่วนตัว ภัยเงียบที่เติบโตในปี 2024",
+            des: "การโจมตีไซเบอร์ที่ใหญ่ที่สุดในไทยส่งผลให้ข้อมูลผู้ใช้งานกว่า 5 ล้านคนรั่วไหล",
+            date: "มกราคม 2567",
+            image: "assets/image/news/news1.png",
+        },
+        {
+            category: "เตือนภัยไซเบอร์",
+            title: "ระบาดหนักอีเมลหลอกลวงพุ่งสูงสุดในรอบปี",
+            des: "การแพร่ระบาดของอีเมลปลอมในชื่อธนาคารดัง หลอกขโมยเงินจากบัญชีผู้ใช้ไปกว่า 100 ล้านบาท",
+            date: "พฤษภาคม 2567",
+            image: "assets/image/news/news2.png",
+        },
+        {
+            category: "กลโกงออนไลน์",
+            title: "ผู้เสียหายจากกลโกงช้อปปิ้งพุ่งสูงในปี 2024",
+            des: "เหตุการณ์มิจฉาชีพหลอกขายสินค้าออนไลน์กว่า 50,000 รายการ โดยไม่มีสินค้าส่งจริง",
+            date: "ตุลาคม 2567",
+            image: "assets/image/news/news3.png",
+        },
+        {
+            category: "เตือนภัยไซเบอร์",
+            title: "เตือนภัย! อีเมลปลอมแอบอ้างหน่วยงานรัฐ",
+            des: "รายงานพบการระบาดของอีเมลปลอมที่แอบอ้างเป็นหน่วยงานรัฐ เพื่อหลอกให้ประชาชนกรอกข้อมูลส่วนตัว เช่น...",
+            date: "มกราคม 2567",
+            image: "assets/image/news/news4.png",
+        },
+        {
+            category: "กลโกงออนไลน์",
+            title: "ระวัง! มิจฉาชีพใช้ QR Code ปลอมดูดเงินในบัญชี",
+            des: "ธนาคารและหน่วยงานด้านความปลอดภัยเตือนภัยการใช้ QR Code ปลอมที่มิจฉาชีพนำไปติดตามสถานที่ต่างๆ...",
+            date: "พฤษภาคม 2567",
+            image: "assets/image/news/news5.png",
+        },
+        {
+            category: "ข่าวประชาสัมพันธ์",
+            title: "เพิ่มความปลอดภัย! หน่วยงานรัฐแนะประชาชนใช้...",
+            des: "หน่วยงานความปลอดภัยทางไซเบอร์ได้แนะนำให้ประชาชน ใช้ระบบยืนยันตัวตนแบบสองชั้นในการเข้าถึงบัญชี",
+            date: "ตุลาคม 2567",
+            image: "assets/image/news/news6.png",
+        },
+    ];
 
-  nextPage(): void {
-    if (this.currentPage < this.paginatedNews.length - 1) {
-      this.currentPage++;
+    paginatedNews: any[][] = [];
+    currentPage = 0;
+    itemsPerPage = 3;
+
+    setupPagination(): void {
+        for (let i = 0; i < this.formationNews.length; i += this.itemsPerPage) {
+            this.paginatedNews.push(
+                this.formationNews.slice(i, i + this.itemsPerPage)
+            );
+        }
     }
-  }
 
-  prevPage(): void {
-    if (this.currentPage > 0) {
-      this.currentPage--;
+    nextPage(): void {
+        if (this.currentPage < this.paginatedNews.length - 1) {
+            this.currentPage++;
+        }
     }
-  }
 
-  navigateToPage(pageIndex: number): void {
-    if (pageIndex >= 0 && pageIndex < this.paginatedNews.length) {
-      this.currentPage = pageIndex;
+    prevPage(): void {
+        if (this.currentPage > 0) {
+            this.currentPage--;
+        }
     }
-  }
 
+    navigateToPage(pageIndex: number): void {
+        if (pageIndex >= 0 && pageIndex < this.paginatedNews.length) {
+            this.currentPage = pageIndex;
+        }
+    }
 }
