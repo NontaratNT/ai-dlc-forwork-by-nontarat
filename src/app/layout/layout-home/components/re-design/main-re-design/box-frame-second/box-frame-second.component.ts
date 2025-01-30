d81aeb809c34290ce181f0ba504f242ff271ee65
Nathaphong.ice <nathaphong.ice@betimes.biz>
2025-01-30 15:24:12 +0700

    update animation

--- src/app/layout/layout-home/components/re-design/main-re-design/box-frame-second/box-frame-second.component.ts
+++ src/app/layout/layout-home/components/re-design/main-re-design/box-frame-second/box-frame-second.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'box-frame-second',
   templateUrl: './box-frame-second.component.html',
   styleUrls: ['./box-frame-second.component.scss']
 })
-export class BoxFrameSecondComponent implements OnInit {
+export class BoxFrameSecondComponent implements OnInit, AfterViewInit {
+
+  isVisible: boolean;
+  @ViewChild('containerDesktop') containerDesktop!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.isVisible = true;
+        } else {
+          this.isVisible = false;
+        }
+      });
+    }, { threshold: 0.2 });
+
+    observer.observe(this.containerDesktop.nativeElement);
   }
 
 }
