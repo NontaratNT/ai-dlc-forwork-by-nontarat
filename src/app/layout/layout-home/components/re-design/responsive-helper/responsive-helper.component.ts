7f38d63a02d6b6408bcc56cf0a821d5647aeb9d3
Nathaphong.ice <nathaphong.ice@betimes.biz>
2025-01-31 17:55:27 +0700

    ีupdate button

--- src/app/layout/layout-home/components/re-design/responsive-helper/responsive-helper.component.ts
+++ src/app/layout/layout-home/components/re-design/responsive-helper/responsive-helper.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'responsive-helper',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponsiveHelperComponent implements OnInit {
 
-  constructor() { }
+  deviceInfo = null;
+
+  constructor(
+    private router: Router,
+    private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
   }
+
+  TelLink(href) {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = href;
+    downloadLink.click();
+  }
+
+  CheckDeviceMode() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    const isMobile = this.deviceService.isMobile();
+    if (isMobile) {
+      this.router.navigate(["/mobile/track-status?openExternalBrowser=1"]);
+    } else {
+      this.router.navigate(["/main/issue-online/1"]);
+    }
+  }
+
+  OnIssueOnline() {
+    console.log("OnIssueOnline");
+    // this.popupVisible = true;
+    this.CheckDeviceMode();
+  }
+
+
 
 }
