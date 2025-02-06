84b6351903ed9b1c434bc0fde06050cecb44293a
Nathaphong.ice <nathaphong.ice@betimes.biz>
2025-02-06 17:08:30 +0700

    update button totop

--- src/app/layout/layout-home/components/re-design/components/totopbtn/totopbtn.component.ts
+++ src/app/layout/layout-home/components/re-design/components/totopbtn/totopbtn.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-totopbtn',
-  templateUrl: './totopbtn.component.html',
-  styleUrls: ['./totopbtn.component.scss']
+    selector: 'app-totopbtn',
+    templateUrl: './totopbtn.component.html',
+    styleUrls: ['./totopbtn.component.scss']
 })
 export class TotopbtnComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  scrollToTop(): void {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
-ngAfterViewInit(): void {
-    window.addEventListener("scroll", () => {
-        const button = document.querySelector(".to-top-btn");
-        if (button) {
-            if (window.scrollY > 150) {
-                button.classList.add("show");
-            } else {
-                button.classList.remove("show");
+    scrollToTop(): void {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    ngAfterViewInit(): void {
+        window.addEventListener("scroll", () => {
+            const button = document.querySelector(".to-top-btn");
+            if (button) {
+                if (window.scrollY > 150) {
+                    button.classList.add("show");
+                } else {
+                    button.classList.remove("show");
+                }
             }
-        }
-    });
-}
+        });
+    }
 
 }
--- src/app/layout/layout-home/components/re-design/responsive-helper/responsive-helper.component.ts
+++ src/app/layout/layout-home/components/re-design/responsive-helper/responsive-helper.component.ts
@@ -44,6 +44,22 @@ export class ResponsiveHelperComponent implements OnInit {
     window.open(link, '_blank');
   }
 
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+  ngAfterViewInit(): void {
+    window.addEventListener("scroll", () => {
+      const button = document.querySelector(".to-top-btn");
+      if (button) {
+        if (window.scrollY > 150) {
+          button.classList.add("show");
+        } else {
+          button.classList.remove("show");
+        }
+      }
+    });
+  }
+
 
 
 }
