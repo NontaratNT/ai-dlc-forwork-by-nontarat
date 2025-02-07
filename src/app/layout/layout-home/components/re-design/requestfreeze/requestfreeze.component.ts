07b3a64fa67e9e0afb2dac2cd8334e55ccd0be15
Arkrapon P. <Arkrapon.p@betimes.biz>
2025-02-07 11:41:10 +0700

    update res request freeze

--- src/app/layout/layout-home/components/re-design/requestfreeze/requestfreeze.component.ts
+++ src/app/layout/layout-home/components/re-design/requestfreeze/requestfreeze.component.ts
@@ -1,99 +1,114 @@
-import { Component, OnInit } from '@angular/core';
-import { NewsService } from 'src/app/services/re-design/news/news.service';
+import { Component, OnInit } from "@angular/core";
+import { NewsService } from "src/app/services/re-design/news/news.service";
 @Component({
-  selector: 'app-requestfreeze',
-  templateUrl: './requestfreeze.component.html',
-  styleUrls: ['./requestfreeze.component.scss']
+    selector: "app-requestfreeze",
+    templateUrl: "./requestfreeze.component.html",
+    styleUrls: ["./requestfreeze.component.scss"],
 })
 export class RequestfreezeComponent implements OnInit {
+    constructor(private service: NewsService) {}
 
-  constructor(private service: NewsService) { }
+    paginatedBanks: any[] = [];
+    currentPage = 0;
+    itemsPerPage = 8;
+    bankContacts: any[] = [];
+    orderBy = false;
+    bankContactsMain: any[] = [];
 
-
-  paginatedBanks: any[] = [];
-  currentPage = 0;
-  itemsPerPage = 8;
-  bankContacts: any[] = [];
-  orderBy = false;
-  bankContactsMain: any[] = [];
-
-  ngOnInit(): void {
-    this.service.getBankList().subscribe((res: any) => {
-      this.bankContacts = res.Value;
-      this.bankContactsMain = res.Value;
-      this.setupPagination();
-    });
-    this.setupPagination();
-  }
-
-  setupPagination(): void {
-    this.paginatedBanks = [];
-    for (let i = 0; i < this.bankContacts.length; i += this.itemsPerPage) {
-      this.paginatedBanks.push(this.bankContacts.slice(i, i + this.itemsPerPage));
-    }
-  }
-
-  nextPage(): void {
-    if (this.currentPage < this.paginatedBanks.length - 1) {
-      this.currentPage++;
-    }
-  }
-
-  prevPage(): void {
-    if (this.currentPage > 0) {
-      this.currentPage--;
-    }
-  }
-
-  navigateToPage(pageIndex: number): void {
-    if (pageIndex >= 0 && pageIndex < this.paginatedBanks.length) {
-      this.currentPage = pageIndex;
-    }
-  }
-
-  // bankContacts = [
-  //   { name: 'กสิกรไทย', phone: '0-2888-8888 กด 001', logo: 'assets/icon/kbank.png' },
-  //   { name: 'ธนาคารกรุงไทย', phone: '0-2111-1111 กด 108', logo: 'assets/icon/ktb.png' },
-  //   { name: 'ธนาคารกรุงศรีอยุธยา', phone: '1572 กด 5', logo: 'assets/icon/bay.png' },
-  //   { name: 'ธนาคารกรุงเทพ', phone: '1333 หรือ 0-2645-5555 กด * 3', logo: 'assets/icon/bbl.png' },
-  //   { name: 'ธนาคารไทยพาณิชย์', phone: '0-2777-7575', logo: 'assets/icon/scb.png' },
-  //   { name: 'ธนาคารทหารไทยธนชาต', phone: '1428 กด 03', logo: 'assets/icon/ttb.png' },
-  //   { name: 'ธนาคารออมสิน', phone: '1115 กด 6', logo: 'assets/icon/gsb.png' },
-  //   { name: 'ธนาคารซีไอเอ็มบี ไทย', phone: '0-2626-7777 กด 12', logo: 'assets/icon/cimb.png' },
-  //   { name: 'ธนาคารไทยเครดิต', phone: '0-2697-5454', logo: 'assets/icon/tcrb.png' },
-  //   { name: 'ธนาคารแลนด์ แอนด์ เฮ้าส์', phone: '0-2459-0000 กด 8', logo: 'assets/icon/lhb.png' },
-  //   { name: 'ธนาคารอาคารสงเคราะห์', phone: '0-2645-9000 กด 33', logo: 'assets/icon/ghb.png' },
-  //   { name: 'ธนาคารเพื่อการเกษตรและสหกรณ์', phone: '0-2888-8888 กด 001', logo: 'assets/icon/baac.png' },
-  //   { name: 'ธนาคารยูโอบี', phone: '0-2344-9555', logo: 'assets/icon/uob.png' },
-  //   { name: 'ธนาคารเกียรตินาคินภัทร', phone: '0-2165-5555 กด 6', logo: 'assets/icon/kk.png' },
-  //   { name: 'ธนาคารทิสโก้', phone: '0-2633-6000 กด * 7', logo: 'assets/icon/tisco.png' },
-  //   { name: 'ธนาคารไอซีบีซี(ไทย)', phone: '0 2629 5588 กด 4', logo: 'assets/icon/icbc.png' },
-  //   { name: 'ธนาคารอิสลามแห่งประเทศไทย', phone: '1302 กด 6', logo: 'assets/icon/ibank.png' },
-  //   { name: 'ทรูมันนี่', phone: '1240 กด 6', logo: 'assets/icon/true.png' },
-  //   { name: 'ทูซีทูพี(ประเทศไทย)', phone: '0 2026 3000 กด 0', logo: 'assets/icon/2C2P.png' },
-  //   { name: 'แอดวานซ์ เอ็มเปย์', phone: '0 2078 9299 กด 1', logo: 'assets/icon/mpay.png' },
-  //   { name: 'ธนาคารไทยไมโคร ดิจิทัล โซลูชั่นส์', phone: '0 2697 5353 กด 0', logo: 'assets/icon/tmc.jpg' },
-  //   { name: 'แมกซ์ การ์ด', phone: '1614 กด 4', logo: 'assets/icon/max.jpg' },
-  // ];
-
-  sortBanksList(): void {
-    this.orderBy = !this.orderBy;
-    if(this.orderBy) {
-      this.bankContacts.sort((a, b) => b.ContactSeq - a.ContactSeq); 
-    }else{
-      this.bankContacts.sort((a, b) => a.ContactSeq - b.ContactSeq); 
-    }
-    this.setupPagination();
-  }
-
-  searchBanksList(event: any): void {
-    const searchValue = event.target.value;
-    if(searchValue){
-        this.bankContacts = this.bankContactsMain.filter((bank: any) => bank.ContactBankName.includes(searchValue));
+    ngOnInit(): void {
+        this.service.getBankList().subscribe((res: any) => {
+            this.bankContacts = res.Value;
+            this.bankContactsMain = res.Value;
+            this.setupPagination();
+        });
         this.setupPagination();
-    }else{
-      this.bankContacts = this.bankContactsMain;
-      this.setupPagination();
     }
-  }
+
+    setupPagination(): void {
+        this.paginatedBanks = [];
+        for (let i = 0; i < this.bankContacts.length; i += this.itemsPerPage) {
+            this.paginatedBanks.push(
+                this.bankContacts.slice(i, i + this.itemsPerPage)
+            );
+        }
+    }
+
+    // ปรับให้ใช้งานได้กับ next และ prev
+    nextPage(): void {
+        if (this.currentPage < this.paginatedBanks.length - 1) {
+            this.animatePageChange(() => {
+                this.currentPage++;
+            }, "next");
+        }
+    }
+
+    prevPage(): void {
+        if (this.currentPage > 0) {
+            this.animatePageChange(() => {
+                this.currentPage--;
+            }, "prev");
+        }
+    }
+
+    navigateToPage(pageIndex: number): void {
+        if (
+            pageIndex >= 0 &&
+            pageIndex < this.paginatedBanks.length &&
+            pageIndex !== this.currentPage
+        ) {
+            const direction = pageIndex > this.currentPage ? "next" : "prev";
+
+            this.animatePageChange(() => {
+                this.currentPage = pageIndex;
+            }, direction);
+        }
+    }
+
+    animatePageChange(callback: () => void, direction: "next" | "prev"): void {
+        const bankList = document.querySelector(".bank-list");
+        if (bankList) {
+            // กำหนด class ให้ตรงกับทิศทาง
+            const outClass =
+                direction === "next" ? "slide-out-left" : "slide-out-right";
+            const inClass =
+                direction === "next" ? "slide-in-right" : "slide-in-left";
+
+            // เพิ่ม animation ออกจากหน้าปัจจุบัน
+            bankList.classList.add(outClass);
+
+            setTimeout(() => {
+                callback(); // เปลี่ยนหน้า
+
+                bankList.classList.remove(outClass);
+                bankList.classList.add(inClass);
+
+                setTimeout(() => {
+                    bankList.classList.remove(inClass);
+                }, 800); // รอ animation จบ
+            }, 800);
+        }
+    }
+
+    sortBanksList(): void {
+        this.orderBy = !this.orderBy;
+        if (this.orderBy) {
+            this.bankContacts.sort((a, b) => b.ContactSeq - a.ContactSeq);
+        } else {
+            this.bankContacts.sort((a, b) => a.ContactSeq - b.ContactSeq);
+        }
+        this.setupPagination();
+    }
+
+    searchBanksList(event: any): void {
+        const searchValue = event.target.value;
+        if (searchValue) {
+            this.bankContacts = this.bankContactsMain.filter((bank: any) =>
+                bank.ContactBankName.includes(searchValue)
+            );
+            this.setupPagination();
+        } else {
+            this.bankContacts = this.bankContactsMain;
+            this.setupPagination();
+        }
+    }
 }
